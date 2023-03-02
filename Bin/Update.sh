3614fe665711fa00528308cdcffd88239ad3fc1c
9Christian6 <chrissi.schulz@googlemail.com>
2023-03-02 14:18:58 +0100

    use firefox for calendar, add prompts for updates

--- Bin/Update.sh
+++ Bin/Update.sh
@@ -3,6 +3,13 @@
 # Update packages
 echo -e "Starting apt-get update\n"
 sudo apt-get update && sudo apt-get upgrade 
+# Prompt user to call autoremove
+read -p "call apt autoremove? (Y/n): " yn
+# If user entered "n", exit the script without clearing the screen
+if [[ "$yn" == "n" ]]; then
+    exit 0
+fi
+sudo apt autoremove
 echo -e "apt-get update done"
 echo -e "---------------------------------------------------------------------"
 echo -e "\nStarting snap update\n"
