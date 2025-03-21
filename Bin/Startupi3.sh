48493434d0c5911006c45c4d0e5296c0d0935a84
Christian Schulz <chrissi.schulz@googlemail.com>
2025-03-21 09:52:28 +0100

    Use script to kill and restart workstyle on login

--- Bin/Startupi3.sh
+++ Bin/Startupi3.sh
@@ -7,15 +7,17 @@
 exec $HOME/Bin/connectionTest.sh&
 wait
 exec picom&
-exec $HOME/.cargo/bin/workstyle&
+#exec $HOME/.cargo/bin/workstyle&
+exec $HOME/Bin/startWorkstyle.sh&
 exec $HOME/Bin/StartAnimation.sh&
 python3 $HOME/Bin/inactiveOpacityException.py&
+exec /usr/bin/gnome-calendar&
 exec spotify&
 exec $HOME/Bin/TerminalStartupi3.sh&
 exec $HOME/Bin/Mails&
 exec $HOME/Bin/StartSocials.sh&
 exec i3-msg "workspace 1"
-sleep 15 && exec $HOME/Bin/removeUrgency
+sleep 60 && exec $HOME/Bin/removeUrgency
 #exec swaymsg 'workspace 1'
 #exec $HOME/Bin/FloatingTab.sh&
 #exec $HOME/Bin/Calendar.sh&
