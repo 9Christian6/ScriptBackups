17f17d87db9d095c72b27df863ab1ec46ffdaf62
Christian Schulz <chrissi.schulz@googlemail.com>
2025-12-19 13:10:59 +0100

    Start different applications

--- Bin/Startupi3.sh
+++ Bin/Startupi3.sh
@@ -1,15 +1,6 @@
 #!/bin/bash	
-exec $HOME/Bin/connectionTest.sh&
-wait
-exec $HOME/Opt/picom-animations/build/src/picom --config /home/christian/.config/picom/picom.conf --experimental-backends --animations --glx-no-stencil --glx-no-rebind-pixmap -b&
-exec $HOME/Bin/startWorkstyle.sh&
-exec $HOME/Bin/StartAnimation.sh&
-exec $HOME/Bin/FloatingTab.sh&
-python3 $HOME/Bin/inactiveOpacityException.py&
-exec /usr/bin/gnome-calendar&
+exec /home/christian/Bin/StartSocials.sh&
+exec /home/christian/Bin/StartupTerminali3.sh&
 exec spotify&
-exec $HOME/Bin/TerminalStartupi3.sh&
-exec $HOME/Bin/Mails&
-exec $HOME/Bin/StartSocials.sh&
-exec /snap/bin/teams-for-linux&
-exec /snap/bin/todoist&
+exec /home/christian/Bin/Mails&
+exec /home/christian/Bin/Calendar.sh&
