9d8d3d75797a16e7945d366c81ee55dba89d2e84
Christian Schulz <chrissi.schulz@googlemail.com>
2025-12-19 13:13:42 +0100

    delete terminal startup scripts that were no longer in use

--- Bin/TerminalStartupi3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash	
-#i3-sensible-terminal & sleep 5 && i3-msg '[instance=x-terminal-emulator] move to workspace 4'
-alacritty&
-sleep 4 
-#exec i3-msg '[title="christian@christiansDesktop:~"] move to workspace 4'
-exec i3-msg '[class="Alacritty"] move to workspace 4'
