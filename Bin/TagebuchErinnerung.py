86c714a5171d425657d9fe72955a4c003ef73a00
Christian Schulz <chrissi.schulz@googlemail.com>
2025-12-19 13:11:42 +0100

    fix TagebuchErinnerungs script

--- Bin/TagebuchErinnerung.py
+++ Bin/TagebuchErinnerung.py
@@ -1,23 +1,28 @@
 #!/bin/python3
-import os
 import datetime
-import calendar
 
-with open('/home/christian/Opt/TagebuchErinnerung.txt') as f:
-    for line in f:
-        pass
-    last_line = line
-last_line = last_line.rstrip()
-lastLineDate = datetime.datetime.strptime(last_line, '%d.%m.%Y')
-today = datetime.datetime.today().strftime('%d.%m.%Y')
-todayDate = datetime.datetime.strptime(today, '%d.%m.%Y')
-dateDiff = todayDate - lastLineDate
-if dateDiff.days == 0:
+
+def get_last_date():
+    with open('/home/christian/Documents/TagebuchErinnerung.txt') as f:
+        for line in f:
+            pass
+        last_line = line
+    return datetime.datetime.strptime(last_line.rstrip(), '%d.%m.%Y')
+
+
+def main():
+    last_line = get_last_date()
+    dateDiff = datetime.datetime.today() - last_line
+    if dateDiff.days > 1:
+        print("Tagebuch")
+        exit()
+    if dateDiff.days == 1:
+        if datetime.datetime.now().hour >= 20:
+            print("Tagebuch")
+            exit()
+    print("")
     exit()
-if dateDiff.days > 1:
-    print("Tagebuch")
-    exit()
-if datetime.datetime.now().hour >= 17:
-    print("Tagebuch")
-    exit()
-exit()
+
+
+if __name__ == "__main__":
+    main()
