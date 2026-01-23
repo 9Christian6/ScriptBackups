a0ca54ddd5f8f6217dfb78dbaf51fced946e2025
Christian Schulz <christian.schulz1996@proton.me>
2026-01-23 13:31:28 +0100

    Fix bug in i3-msg command

--- Bin/newFreeWorkspace.py
+++ Bin/newFreeWorkspace.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 import subprocess
 import json
+import sys
 from typing import Sequence
 
 def run_i3_msg(command: Sequence[str]) -> str:
@@ -18,11 +19,14 @@ def run_i3_msg(command: Sequence[str]) -> str:
 def focus_workspace(name: str) -> None:
     """Focus a workspace by name."""
     global commandWasFromHistory
-    run_i3_msg([f'workspace "{name}:{name}"'])
+    run_i3_msg([f'workspace number "{name}:{name}"'])
 
+def move_to_workspace(name: str) -> None:
+    """Move currently focused container to workspace"""
+    run_i3_msg([f'move container to workspace number "{name}:{name}"'])
 
 def main() -> int:
-    miscWorkspaces = ['3', '6', '7', '14', '15', '16', '17', '18', '19']
+    miscWorkspaces = ['1', '3', '6', '7', '14', '15', '16', '17', '18', '19']
     workspaces = subprocess.check_output(['i3-msg', '-t', 'get_workspaces'])
     workspacesDict = json.loads(workspaces)
     occupiedWorkspaces = []
@@ -39,7 +43,11 @@ def main() -> int:
         if item not in occupiedWorkspaces:
             freeWorkspaces.append(item)
     if freeWorkspaces.__len__() > 0:
-        focus_workspace(freeWorkspaces[0])
+        if sys.argv.__len__() > 1:
+            move_to_workspace(freeWorkspaces[0])
+            focus_workspace(freeWorkspaces[0])
+        else:
+            focus_workspace(freeWorkspaces[0])
     print(freeWorkspaces)
     return 0
 
